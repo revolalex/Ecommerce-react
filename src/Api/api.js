dc1e487374cdc416b331297c35075108a5570052
Alex <revolalex@gmail.com>
2020-10-07 12:06:48 +0200

    db finish

---
+++ src/Api/api.js
@@ -0,0 +1,35 @@
+/************************** Module ****************************/
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+var cors = require("cors");
+
+/************************** MidlleWare ****************************/
+app.use(express.urlencoded({ extended: false }));
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CORS middleware
+app.use(cors());
+const allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+};
+app.use(allowCrossDomain);
+
+/************************** connection mysql ****************************/
+const connection = require("./database/db");
+connection.connect();
+
+/************************** Routes ****************************/
+// require("./routes/route")(app, connection);
+
+//connection.end();
+
+app.listen(3000, function() {
+  console.log("server listening on: http://localhost:3000/sign-up");
+});
---
+++ src/Api/database/db.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "alexandre",
+  database: "ecomreact",
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected to DB");
+
+  let createDB = "CREATE database IF NOT EXISTS ecomreact";
+  connection.query(createDB, function(err, results) {
+    if (err) throw err;
+  });
+
+  let userTable =
+    "CREATE TABLE IF NOT EXISTS users ( id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, email VARCHAR(200) NOT NULL, pass VARCHAR(200) NOT NULL)";
+  connection.query(userTable, function(err, results) {
+    if (err) throw err;
+  });
+
+  let productTable =
+    "CREATE TABLE IF NOT EXISTS products ( id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, category VARCHAR(30) NOT NULL, name VARCHAR(30) NOT NULL, description VARCHAR(200) NOT NULL, prices INT(10) NOT NULL)";
+  connection.query(productTable, function(err, results) {
+    if (err) throw err;
+  });
+});
+
+module.exports = connection;
