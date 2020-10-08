e0600a21a1cdd0c49dff2b3f159dc0e38a789740
Alex <revolalex@gmail.com>
2020-10-08 15:07:52 +0200

    css change for navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import "./App.css";
-import {Route, Switch } from 'react-router-dom';
-
-import NavBarComponent from "./Components/Login"
 import LoginComponent from "./Components/Login";
 import SignupComponent from "./Components/Signup";
-import AddingProductComponent from "./Components/AddingProductComponent"
+// import AddingProductComponent from "./Components/AddingProductComponent"
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+import NavBarComponent from './Components/NavBarComponent'
 
 class App extends Component {
   render() {
     return (
-
-      <main>
-        {/* <NavBarComponent/> */}
-        <Switch>
-          <Route path="/" component={NavBarComponent} exact />
-          <Route path="/signup" component={SignupComponent} exact />
-          <Route path="/signin" component={LoginComponent} />
-          <Route path="/product" component={AddingProductComponent} />
-        </Switch>
-      </main>
+      <Router>
+        <div  id="myNav">
+          <NavBarComponent/>
+          {/* <ul>
+            <button>
+              <Link to="/">Sign up</Link>
+            </button>
+            <button>
+              <Link to="/Sign-in">Sign in</Link>
+            </button>
+          </ul> */}
+          </div>
+          <Switch>
+            <Route path="/Sign-in">
+              <LoginComponents/>
+            </Route>
+            <Route path="/">
+              <SignupComponents/>  
+            </Route>           
+          </Switch>
+      </Router>
     );
   }
+}
+
+function SignupComponents() {
+  return <SignupComponent/>
+}
+
+function LoginComponents() {
+  return <LoginComponent/>
 }
 
 export default App;
--- src/Components/NavBarComponent.js
+++ src/Components/NavBarComponent.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 class NavBarComponent extends Component {
   render() {
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
-    </Navbar>;
+    return (
+      <main>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Sign-Up</Nav.Link>
+            <Nav.Link href="/sign-in">Sign-In</Nav.Link>
+            <Nav.Link href="/product">Product</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search Product</Button>
+          </Form>
+        </Navbar>
+      </main>
+    );
   }
 }
-export default NavBarComponent
+export default NavBarComponent;
