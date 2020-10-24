8c64a4704b92156d28e0e103a47b719ba5330498
Alex <revolalex@gmail.com>
2020-10-25 00:05:13 +0200

    reset cart on sign-out

--- src/Components/Navbar/NavBarComponent.js
+++ src/Components/Navbar/NavBarComponent.js
@@ -5,6 +5,7 @@ import MyRouter from "../Router";
 import { deleteToken, authFalse } from "../../store/actions/user";
 import { resetHistoryOrders } from "../../store/actions/orders";
 import { resetCategory } from "../../store/actions/category";
+import { resetCart } from "../../store/actions/cart";
 import { connect } from "react-redux";
 import WaveAnimationComponent from "../Animation/WaveAnimation";
 import NavbarWithToken from "./NavbarWithToken";
@@ -14,16 +15,21 @@ class Navbare extends Component {
   deleteToken() {
     this.props.authFalse();
     this.props.deleteToken();
-    this.props.history.push("/");
     this.props.resetHistoryOrders();
     this.props.resetCategory();
+    this.props.resetCart();
+    this.props.history.push("/");
   }
 
   render() {
     return (
       <div>
         <WaveAnimationComponent />
-        {this.props.token ? <NavbarWithToken onClick={this.deleteToken.bind(this)} /> : <NavbarWithoutToken />}
+        {this.props.token ? (
+          <NavbarWithToken onClick={this.deleteToken.bind(this)} />
+        ) : (
+          <NavbarWithoutToken />
+        )}
         <MyRouter />
       </div>
     );
@@ -39,6 +45,7 @@ const mapDispatchToProps = {
   authFalse,
   resetHistoryOrders,
   resetCategory,
+  resetCart,
 };
 export default connect(
   mapStateToProps,
